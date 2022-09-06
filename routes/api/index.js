77c100e446eadda81129ce29994418194df89729
samsiebert <sammsiebert@gmail.com>
2022-09-05 20:32:44 -0500

    file set-up and initial server and connection code

---
+++ routes/api/index.js
@@ -0,0 +1,10 @@
+const router = require('express').Router();
+const postRoutes = require('./post-routes');
+
+const commentRoutes = require('./comment-routes');
+
+router.use('/comments', commentRoutes);
+
+router.use('/posts', postRoutes);
+
+module.exports = router;
